39553d155aa958149d7808d3d49d94c20ac49ec1
junhyung85920 <junhyung85920@gmail.com>
2024-11-15 16:09:13 +0900

    Feat: Add streaming method for static video file

--- src/main/java/gdg/farm_in_palm/domain/monitor/service/MonitorService.java
+++ src/main/java/gdg/farm_in_palm/domain/monitor/service/MonitorService.java
@@ -9,14 +9,16 @@ import gdg.farm_in_palm.exception.CustomException;
 import gdg.farm_in_palm.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +26,7 @@ import java.util.List;
 public class MonitorService {
 
     private final MonitorRepository monitorRepository;
+    private final String URL = "src/main/resources/static";
 
     // 모든 Monitor 조회
     public List<MonitorInfoResDTO> getAllMonitorsByUserId(Long userId) {
@@ -85,28 +88,35 @@ public class MonitorService {
                 .build();
     }
 
-    // streaming
-    public ResponseEntity<FileSystemResource> streamVideo(String videoName, HttpHeaders headers) throws IOException {
+    // streaming (static folder에 저장된 video stream)
+    public ResponseEntity<ResourceRegion> streamVideo(HttpHeaders headers) throws IOException {
 
-        File videoFile = new File("/Users/junhyung85920/Downloads/" + videoName);
-        long fileSize = videoFile.length();
-        List<HttpRange> ranges = headers.getRange();
+        String path = URL + "/test.mp4";
+        Resource resource = new FileSystemResource(path);
 
-        if (!ranges.isEmpty()) {
-            HttpRange range = ranges.get(0);
-            long start = range.getRangeStart(fileSize);
-            long end = range.getRangeEnd(fileSize);
-            FileSystemResource resource = new FileSystemResource(videoFile);
+        long chunkSize = 1024 * 1024;
+        long contentLength = resource.contentLength();
 
-            return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
-                    .header(HttpHeaders.CONTENT_RANGE, "bytes " + start + "-" + end + "/" + fileSize)
-                    .contentLength(end - start + 1)
-                    .body(resource);
-        } else {
-            return ResponseEntity.ok()
-                    .contentLength(fileSize)
-                    .contentType(MediaType.parseMediaType("video/mp4"))
-                    .body(new FileSystemResource(videoFile));
+        ResourceRegion region;
+
+        try {
+            HttpRange httpRange = headers.getRange().stream().findFirst().get();
+            long start = httpRange.getRangeStart(contentLength);
+            long end = httpRange.getRangeEnd(contentLength);
+            long rangeLength = Long.min(chunkSize, end -start + 1);
+
+            region = new ResourceRegion(resource, start, rangeLength);
+        } catch (Exception e) {
+            long rangeLength = Long.min(chunkSize, contentLength);
+            region = new ResourceRegion(resource, 0, rangeLength);
         }
+
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES))
+                .contentType(MediaTypeFactory.getMediaType(resource).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .header("Accept-Ranges", "bytes")
+                .eTag(path)
+                .body(region);
+
     }
 }
